09450ec5e9dab74e7f1fd3d1c4fd385431b89d7b
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2025-03-19 09:25:05 -0400

    JMK_31: setup spring boot project

---
+++ src/main/java/com/jobmatrix/JmJobPostingApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmJobPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmJobPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobmatrix/JmJobPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmJobPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
